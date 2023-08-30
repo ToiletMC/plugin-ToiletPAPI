6f4a91b1884e490b75e92293307714e3e80c913f
TheLittle_Yang <2570984321@qq.com>
2023-08-30 20:02:21 +0800

    update

--- src/main/java/net/toiletmc/toiletpapi/ToiletPAPI.java
+++ src/main/java/net/toiletmc/toiletpapi/ToiletPAPI.java
@@ -1,14 +1,89 @@
 package net.toiletmc.toiletpapi;
 
+import me.clip.placeholderapi.expansion.Configurable;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.lucko.spark.api.Spark;
+import me.lucko.spark.api.statistic.StatisticWindow;
+import me.lucko.spark.api.statistic.types.DoubleStatistic;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ToiletPAPI extends PlaceholderExpansion {
+import java.util.HashMap;
+import java.util.Map;
+
+public class ToiletPAPI extends PlaceholderExpansion implements Configurable {
+    private String footer;
+    private String fixedWorldEmoji;
+    private Spark spark;
+
+    @Override
+    public Map<String, Object> getDefaults() {
+        Map<String, Object> defaults = new HashMap<>();
+        defaults.put("footer", "请修改默认页脚信息");
+        defaults.put("fixed-world-emoji", "none");
+        return defaults;
+    }
+
+    @Override
+    public boolean register() {
+        RegisteredServiceProvider<Spark> provider = Bukkit.getServicesManager().getRegistration(Spark.class);
+        if (provider != null) {
+            spark = provider.getProvider();
+            this.info("已挂钩到 Spark");
+        } else {
+            this.severe("spark 服务异常，已停用插件");
+            this.unregister();
+        }
+
+        footer = this.getString("footer", "请修改默认页脚信息");
+        fixedWorldEmoji = this.getString("fixed-world-emoji", "none");
+
+        return super.register();
+    }
+
     @Override
     public @Nullable String onPlaceholderRequest(Player player, @NotNull String params) {
+        switch (params) {
+            case "worldemoji" -> {
+                if (!fixedWorldEmoji.equals("none")) {
+                    return fixedWorldEmoji;
+                }
 
+                switch (player.getWorld().getName()) {
+                    case "resource_world" -> {
+                        return "§7⛏";
+                    }
+                }
+            }
+            case "tpsemoji" -> {
+                DoubleStatistic<StatisticWindow.TicksPerSecond> tps = spark.tps();
+                double tpsLast10Secs = tps.poll(StatisticWindow.TicksPerSecond.SECONDS_10);
+                if (tpsLast10Secs >= 18) {
+                    return "§a\uD83C\uDF11";
+                } else if (tpsLast10Secs >= 15) {
+                    return "§6\uD83C\uDF11";
+                } else if (tpsLast10Secs >= 10) {
+                    return "§c\uD83C\uDF11";
+                } else {
+                    return "§4\uD83C\uDF11";
+                }
+            }
+            case "worldtimeemoji" -> {
+                Long gameTime = player.getWorld().getTime() % 24000;
+                if (gameTime >= 0 && gameTime <= 12000) {
+                    return "§6☀";
+                } else {
+                    return "§e\uD83C\uDF1A";
+                }
+            }
+            case "footer" -> {
+                return footer;
+            }
+        }
+        return "";
     }
 
     @Override
